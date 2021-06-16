a6b1be9dfba19f5cf8ed227f8710628ddb12edba
“Pavel <itspavelnosarev@gmail.com>
2021-06-16 14:57:03 -0400

    added new seed for store

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def product_first
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def all_products
+    product = Product.all
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/product_first", controller: "products", action: "product_first"
+  get "all_products", controller: "products", action: "all_products"
 end
