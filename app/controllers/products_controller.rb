1e26fa5244578a9740573f18ba72213b2372fa21
“Pavel <itspavelnosarev@gmail.com>
2021-06-17 15:20:34 -0400

    added route and action for one product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,14 @@ class ProductsController < ApplicationController
   end
 
   def all_products
+    input = params["product"]
     product = Product.all
+    render json: product.as_json
+  end
+
+  def one_product
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
     render json: product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "/product_first", controller: "products", action: "product_first"
-  get "all_products", controller: "products", action: "all_products"
+  #get "/product_first", controller: "products", action: "product_first"
+  #get "all_products", controller: "products", action: "all_products"
+  get "/one_product" => "products#one_product"
 end
